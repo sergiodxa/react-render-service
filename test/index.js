6780fcfcc83fe88a1a1ff916e86ae6d28e746532
Sergio Daniel Xalambrí <daniel@platzi.com>
2016-02-26 21:20:52 -0500

    [add] tests

---
+++ test/index.js
@@ -0,0 +1,22 @@
+const test = require('tape');
+const cheerio = require('cheerio');
+const superagent = require('superagent-as-promised')(require('superagent'));
+
+test('Render service', t => {
+  t.plan(1);
+
+  const expectedResult = 'Hello world!';
+
+  superagent
+    .post('http://localhost:3000/')
+    .send({
+      component: './test/HelloWorld.js',
+    })
+    .then(response => {
+      t.equal(
+        cheerio.load(response.text)('h1').text(),
+        expectedResult
+      );
+    })
+    .catch(error => t.error(error));
+});
